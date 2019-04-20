7104257f1ab89776676ac5b9930dfd90d22df3bb
alicek106 <alice_k106@naver.com>
2019-04-20 14:40:41 +0900

    Fix missed return value when connect to just started instance.

--- __main__.py
+++ __main__.py
@@ -5,12 +5,11 @@ from aws_ec2_manager import AwsEc2Manager
 import botocore
 import subprocess
 
-"""
+
 logging.basicConfig(
     format='%(asctime)s %(levelname)-8s %(message)s',
     level=logging.INFO,
     datefmt='%Y-%m-%d %H:%M:%S')
-"""
 
 env_parser = EnvParser()
 
@@ -22,17 +21,20 @@ def check_credentials():
         logging.error('AWS Credential is not set in environment variable')
         exit(102)
 
+
 def command_start(aws_ec2_manager, arg):
     logging.info('Starting EC2 instance : {}'.format(arg))
-    public_ip_address = aws_ec2_manager.start_instance()
+    aws_ec2_manager.start_instance(ec2_instance_name=arg)
+    public_ip_address = aws_ec2_manager.check_instance_running(ec2_instance_name=arg, max_tries=30, warmup_time=30)
+
     cmd = ['ssh', '-oStrictHostKeyChecking=no',
-           '-i{}'.format(env_parser.EC2_SSH_PRIVATE_KEY), 'ubuntu@{}'.format(public_ip_address)]
+       '-i{}'.format(env_parser.EC2_SSH_PRIVATE_KEY), 'ubuntu@{}'.format(public_ip_address)]
     subprocess.call(cmd)
 
 
 def command_stop(aws_ec2_manager, arg):
     logging.info('Stopping EC2 instance : {}'.format(arg))
-    aws_ec2_manager.stop_instance()
+    aws_ec2_manager.stop_instance(arg)
 
 
 if __name__ == "__main__":
