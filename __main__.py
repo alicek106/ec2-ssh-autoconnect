e5480c49a8cf92d99823779a0cc815737a97185b
alicek106 <alice_k106@naver.com>
2019-04-20 14:02:44 +0900

    Fix monolithic design to manage multiple ec2 instance.

--- __main__.py
+++ __main__.py
@@ -1,76 +1,41 @@
-import awsutils
-import time
 import logging
-import os
 import sys
 from env_parser import EnvParser
+from aws_ec2_manager import AwsEc2Manager
 import botocore
 import subprocess
 
+"""
 logging.basicConfig(
     format='%(asctime)s %(levelname)-8s %(message)s',
     level=logging.INFO,
     datefmt='%Y-%m-%d %H:%M:%S')
+"""
+
+env_parser = EnvParser()
 
 
-class StatusCode:
-    STOPPED = 80
-    STOPPING = 64
-    RUNNING = 16
+def check_credentials():
+    try:
+        return AwsEc2Manager()
+    except botocore.exceptions.NoCredentialsError as e:
+        logging.error('AWS Credential is not set in environment variable')
+        exit(102)
+
+def command_start(aws_ec2_manager, arg):
+    logging.info('Starting EC2 instance : {}'.format(arg))
+    public_ip_address = aws_ec2_manager.start_instance()
+    cmd = ['ssh', '-oStrictHostKeyChecking=no',
+           '-i{}'.format(env_parser.EC2_SSH_PRIVATE_KEY), 'ubuntu@{}'.format(public_ip_address)]
+    subprocess.call(cmd)
 
 
-class AwsEc2Manager:
-    client = None
-    ec2_instance_data = None
-    ec2_instance_name = None
-
-    def __init__(self, ec2_instance_name):
-        session = awsutils.get_session('ap-northeast-2')
-        self.client = session.client('ec2')
-        self.ec2_instance_name = ec2_instance_name
-        self.__update_instance_data(ec2_instance_name)
-
-        if self.ec2_instance_data['State']['Code'] != StatusCode.RUNNING and \
-                self.ec2_instance_data['State']['Code'] != StatusCode.STOPPED:
-            print('Instance is stopping or staring. Try again after few seconds.')
-            exit(100)
-
-    def __update_instance_data(self, instance_name):
-        instance_data = self.client.describe_instances(
-            Filters=[
-                {'Name': 'tag:Name', 'Values': [instance_name]}
-            ]
-        )
-        self.ec2_instance_data = instance_data['Reservations'][0]['Instances'][0]
-
-    def start_instance(self):
-        # Check whether instance is already running
-        if self.ec2_instance_data['State']['Code'] == StatusCode.RUNNING:
-            logging.info('EC2 instance is in active.')
-            return self.ec2_instance_data['PublicIpAddress']
-
-        # It not running, start and wait
-        ec2_instance_id = self.ec2_instance_data['InstanceId']
-        self.client.start_instances(InstanceIds=[ec2_instance_id])
-
-        for i in range(1, 30):
-            time.sleep(1)
-            self.__update_instance_data(self.ec2_instance_name)
-            if self.ec2_instance_data['State']['Code'] == StatusCode.RUNNING:
-                logging.info('EC2 instance is in active. Waiting for 20 seconds for warm-up.')
-                time.sleep(20)
-                return self.ec2_instance_data['PublicIpAddress']
-            logging.info('Waiting for starting EC2 instance.. {} tries'.format(i))
-        logging.error('Failed to start EC2 instance. Max tries : 30')
-        exit(100)
-
-    def stop_instance(self):
-        ec2_instance_id = self.ec2_instance_data['InstanceId']
-        self.client.stop_instances(InstanceIds=[ec2_instance_id])
+def command_stop(aws_ec2_manager, arg):
+    logging.info('Stopping EC2 instance : {}'.format(arg))
+    aws_ec2_manager.stop_instance()
 
 
 if __name__ == "__main__":
-    env_parser = EnvParser()
     args = sys.argv
 
     if len(args) != 3:
@@ -78,22 +43,22 @@ if __name__ == "__main__":
         logging.error('Usage: python __main__.py [command: connect or stop] [ec2-instance-name]')
         exit(101)
 
-    try:
-        awsEc2Manager = AwsEc2Manager(args[2])
-    except botocore.exceptions.NoCredentialsError as e:
-        logging.error('AWS Credential is not set in environment variable')
-        exit(102)
+    aws_ec2_manager = check_credentials()
 
     if args[1] == 'connect':
-        logging.info('Starting EC2 instance : {}'.format(args[2]))
-        public_ip_address = awsEc2Manager.start_instance()
-        cmd = ['ssh', '-oStrictHostKeyChecking=no', 
-                '-i{}'.format(env_parser.EC2_SSH_PRIVATE_KEY), 'ubuntu@{}'.format(public_ip_address)]
-        subprocess.call(cmd)
+        command_start(aws_ec2_manager, args[2])
 
     elif args[1] == 'stop':
-        logging.info('Stopping EC2 instance : {}'.format(args[2]))
-        awsEc2Manager.stop_instance()
+        command_stop(aws_ec2_manager, args[2])
+
+    elif args[1] == 'group_start':
+        # TODO : Implement group start
+        print('group start')
+
+    elif args[1] == 'group_stop':
+        # TODO : Implement group stop
+        print('group stop')
+
     else:
         logging.error("Argumeht should be 'connect' or 'stop'")
         exit(101)
---
+++ aws_ec2_manager.py
@@ -0,0 +1,139 @@
+import awsutils
+import time
+import logging
+import unittest
+import warnings
+
+
+class StatusCode:
+    STOPPED = 80
+    STOPPING = 64
+    RUNNING = 16
+
+
+class AwsEc2Manager_test(unittest.TestCase):
+
+    def setUp(self):
+        warnings.filterwarnings("ignore", category=ResourceWarning, message="unclosed.*<ssl.SSLSocket.*>")
+        self.aws_ec2_manager = AwsEc2Manager()
+
+    def test_start_instance(self):
+        # Start and wait until warm-up
+        self.aws_ec2_manager.start_instance(ec2_instance_name='Test')
+        self.aws_ec2_manager.check_instance_running('Test', max_tries=20, warmup_time=20)
+
+        # Start when instance is active
+        self.aws_ec2_manager.start_instance(ec2_instance_name='Test')
+
+    def test_stop_instance(self):
+        # Stop when instance is active
+        self.aws_ec2_manager.stop_instance(ec2_instance_name='Test')
+        self.aws_ec2_manager.check_instance_stopped(ec2_instance_name='Test', max_tries=20)
+
+        # Stop when instance is stop
+        self.aws_ec2_manager.stop_instance(ec2_instance_name='Test')
+
+
+class AwsEc2Manager():
+
+    def __init__(self):
+        session = awsutils.get_session('ap-northeast-2')
+        self.client = session.client('ec2')
+        """
+        if self.ec2_instance_data['State']['Code'] != StatusCode.RUNNING and \
+                self.ec2_instance_data['State']['Code'] != StatusCode.STOPPED:
+            print('Instance is stopping or staring. Try again after few seconds.')
+            exit(100)
+        """
+
+    def __get_instance_data(self, instance_name):
+
+        """
+        Get detailed metadata of EC2 instance.
+        :param instance_name: Target EC2 instance to describe metadata.
+        :return: Metadata of EC2 instance.
+        """
+
+        instance_data = self.client.describe_instances(
+            Filters=[
+                {'Name': 'tag:Name', 'Values': [instance_name]}
+            ]
+        )
+        return instance_data['Reservations'][0]['Instances'][0]
+
+    def start_instance(self, ec2_instance_name):
+
+        """
+        Start EC2 instance.
+        :param ec2_instance_name: The unique name of EC2 instance to start.
+        :return: If already running, return Public IP. If not, return True.
+        """
+
+        ec2_instance_data = self.__get_instance_data(ec2_instance_name)
+
+        if ec2_instance_data['State']['Code'] == StatusCode.RUNNING:
+            logging.info('EC2 instance is in active.')
+            return ec2_instance_data['PublicIpAddress']
+
+        ec2_instance_id = ec2_instance_data['InstanceId']
+        self.client.start_instances(InstanceIds=[ec2_instance_id])
+        logging.info('Starting EC2 instance {}...'.format(ec2_instance_name))
+        return True
+
+    def check_instance_running(self, ec2_instance_name, max_tries, warmup_time):
+
+        """
+        Check whether EC2 instance is running.
+        :param ec2_instance_name: EC2 instance name to check health.
+        :param max_tries: Seconds to wait until EC2 instance will be active.
+        :param warmup_time: Seconds to wait for warm-up when EC2 instance is in active.
+        :return: If succeeded to start, return Public IP. If not, return False.
+        """
+
+        for i in range(1, max_tries):
+            time.sleep(1)
+            ec2_instance_data = self.__get_instance_data(ec2_instance_name)
+
+            if ec2_instance_data['State']['Code'] == StatusCode.RUNNING:
+                logging.info('EC2 instance is in active. Waiting for {} seconds for warm-up.'.format(warmup_time))
+                time.sleep(warmup_time)
+                return ec2_instance_data['PublicIpAddress']
+
+            logging.info('Waiting for starting EC2 instance.. {} tries'.format(i))
+        logging.error('Failed to start EC2 instance. Max tries : {}'.format(max_tries))
+        return False
+
+    def check_instance_stopped(self, ec2_instance_name, max_tries):
+
+        """
+        Check whether instance is stopped.
+        :param ec2_instance_name: Target EC2 instance to check instance is stopped.
+        :param max_tries: Seconds to wait until EC2 instance will be stopped.
+        :return: If succeeded to stop, return True. If not, return False.
+        """
+
+        for i in range(1, max_tries):
+            time.sleep(1)
+            ec2_instance_data = self.__get_instance_data(ec2_instance_name)
+            if ec2_instance_data['State']['Code'] == StatusCode.STOPPED:
+                return True
+            logging.info('Waiting for stopping EC2 instance.. {} tries'.format(i))
+        logging.error('Failed to wait stopping EC2 instance. Max tries : {}'.format(max_tries))
+        return False
+
+    def stop_instance(self, ec2_instance_name):
+
+        """
+        Stop EC2 instance.
+        :param ec2_instance_name: Target EC2 instance to stop.
+        :return: None
+        """
+
+        ec2_instance_data = self.__get_instance_data(ec2_instance_name)
+        ec2_instance_id = ec2_instance_data['InstanceId']
+        self.client.stop_instances(InstanceIds=[ec2_instance_id])
+        logging.error('Stopping EC2 instance {}...'.format(ec2_instance_name))
+
+
+if __name__ == '__main__':
+    unittest.main()
